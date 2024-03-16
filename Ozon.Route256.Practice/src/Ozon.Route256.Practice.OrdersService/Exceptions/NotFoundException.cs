f8344d24b38c842b5764a860abde13dc409a69e7
Oksana Mushtakova <oxana.mushtakova@gmail.com>
2024-03-16 08:54:17 +0300

    GRPC OrdersService handlers are added(no implementation, only contracts are defined).

---
+++ Ozon.Route256.Practice/src/Ozon.Route256.Practice.OrdersService/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Ozon.Route256.Practice.OrdersService.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public override string Message { get; }
+
+        public NotFoundException(string exceptionMessage)
+        {
+            Message = exceptionMessage;
+        }
+    }
+}
---
+++ Ozon.Route256.Practice/src/Ozon.Route256.Practice.OrdersService/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Ozon.Route256.Practice.OrdersService.Infrastructure;
+
+namespace Ozon.Route256.Practice.OrdersService
+{
+    public sealed class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddGrpc(option => option.Interceptors.Add<LoggerInterceptor>());
+            serviceCollection.AddControllers();
+            serviceCollection.AddEndpointsApiExplorer();
+            serviceCollection.AddSwaggerGen();
+            serviceCollection.AddGrpcReflection();
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseRouting();
+            applicationBuilder.UseSwagger();
+            applicationBuilder.UseSwaggerUI();
+            //applicationBuilder.UseHttpsRedirection();
+            applicationBuilder.UseEndpoints(endpointRouteBuilder =>
+            {
+                endpointRouteBuilder.MapGet("", () => "Hello World!");
+                endpointRouteBuilder.MapGrpcService<GrpcServices.OrdersService>();
+                endpointRouteBuilder.MapGrpcReflectionService();
+            });
+        }
+    }
+}
